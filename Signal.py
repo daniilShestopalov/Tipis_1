4eabdcdeef905b7d0a0eb3385dfb8748ebef76c5
daniilShestopalov <danya.shestopalov@yandex.ru>
2023-09-28 18:16:41 +0300

    Maybe final without click optn

--- Signal.py
+++ Signal.py
@@ -1,4 +1,5 @@
 import enum
+import numpy as np
 from numpy import arange
 from math import cos, sin
 from math import pi
@@ -8,20 +9,20 @@ class SignalState(enum.Enum):
     HARMONIC = 0
     DIGITAL = 1
 
-
 # constants
 AMPLITUDE = 5
-STEP = 0.01
+STEP = 0.001
 PHASE = 0
-MAX_TIME = 10
-
+MAX_TIME = 1
+TIME_OF_SIGNAL = arange(start=0, stop=MAX_TIME, step=STEP)
 
 def sum_for_digital(t, c_f):
     s = 0
     for i in arange(start=1, stop=1000, step=1):
         s += sin(c_f * t * (2 * i - 1)) / (2 * i - 1)
+    if s <= 0:
+        return 0
     return s
-
 
 class Signal(object):
     def __init__(self, frequency: float, signal_type: SignalState):
@@ -32,11 +33,31 @@ class Signal(object):
         cycle_frequency = 2 * pi * self.frequency
         points = [[],[]]
         if self.signal_type == SignalState.HARMONIC:
-            for t in arange(start=0, stop=MAX_TIME, step=STEP):
+            for t in TIME_OF_SIGNAL:
                 points[0].append(t)
-                points[1].append(AMPLITUDE * cos(cycle_frequency * t + PHASE))
+                points[1].append(AMPLITUDE * sin(cycle_frequency * t + PHASE))
         else:
-            for t in arange(start=0, stop=MAX_TIME, step=STEP):
+            for t in TIME_OF_SIGNAL:
                 points[0].append(t)
                 points[1].append(AMPLITUDE * 4 / pi * sum_for_digital(t, cycle_frequency))
+        return points
+
+    def create_arr_of_point_for_spectrum(self):
+        cycle_frequency = 2 * pi * self.frequency
+        points = [[],[]]
+        signal_res = []
+
+        if self.signal_type == SignalState.HARMONIC:
+            for t in TIME_OF_SIGNAL:
+                signal_res.append(AMPLITUDE * sin(cycle_frequency * t + PHASE))
+
+            points[1].append(np.abs(np.fft.fft(signal_res)))
+            points[0].append(np.fft.fftfreq(len(signal_res), d=STEP))
+        else:
+            for t in TIME_OF_SIGNAL:
+                signal_res.append(AMPLITUDE * 4 / pi * sum_for_digital(t, cycle_frequency))
+
+            points[1].append(np.abs(np.fft.fft(signal_res)))
+            points[0].append(np.fft.fftfreq(len(signal_res), d=STEP))
+
         return points
--- main.py
+++ main.py
@@ -2,12 +2,23 @@ from matplotlib import pyplot as plt
 
 import Signal
 
+'''def create_draw_components(code):
+    frequencies = [1, 2, 4, 8]
+    if code == 1:
+
+
+def onclick(event):
+    if event.button == 1:'''
+
+
 if __name__ == '__main__':
     frequencies = [1, 2, 4, 8]
+
     fig, ax = plt.subplots(4, 2, layout='constrained')
 
     harmonic = Signal.Signal(1, Signal.SignalState.HARMONIC)
     digital = Signal.Signal(1, Signal.SignalState.DIGITAL)
+
 
     for i in range(len(frequencies)):
         harmonic.frequency = frequencies[i]
@@ -15,20 +26,22 @@ if __name__ == '__main__':
 
         points_of_harmonic = harmonic.create_arr_of_point_for_signal()
         points_of_digital = digital.create_arr_of_point_for_signal()
+        points_of_harmonic_spectrum = harmonic.create_arr_of_point_for_spectrum()
+        points_of_digital_spectrum = digital.create_arr_of_point_for_spectrum()
 
         ax[i][0].plot(points_of_harmonic[0], points_of_harmonic[1], color='blue', label="Гармонический")
-        ax[i][0].plot(points_of_digital[0], points_of_digital[1], color='orange', label="Цифровой")
-        ax[i][0].set_xlabel('t')
-        ax[i][0].set_ylabel('y')
+        ax[i][0].plot(points_of_digital[0], points_of_digital[1], color='green', label="Цифровой")
+        ax[i][0].set_xlabel('Сек')
+        ax[i][0].set_ylabel('В')
         ax[i][0].set_title('Визуализация сигнала с частатой ' + str(frequencies[i]) + ' Гц')
         ax[i][0].legend(loc='lower left')
 
-        ax[i][1].stem(points_of_harmonic[0][::(4 - i)], points_of_harmonic[1][::(4 - i)], linefmt='blue',
+        ax[i][1].plot(points_of_harmonic_spectrum[0][0], points_of_harmonic_spectrum[1][0], color='blue',
                       label="Гармонический")
-        ax[i][1].stem(points_of_digital[0][::(4 - i)], points_of_digital[1][::(4 - i)], linefmt='green',
+        ax[i][1].plot(points_of_digital_spectrum[0][0], points_of_digital_spectrum[1][0], color='green',
                       label="Цифровой")
-        ax[i][1].set_xlabel('t')
-        ax[i][1].set_ylabel('y')
+        ax[i][1].set_xlabel('Гц')
+        ax[i][1].set_ylabel('В')
         ax[i][1].set_title('Визуализация спектра сигнала с частатой ' + str(frequencies[i]) + ' Гц')
         ax[i][1].legend(loc='lower left')
     plt.show()
